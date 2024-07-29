4418986936b4f340fd2c55dade12f1d0b0c3dbfa
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-29 19:10:07 +0530

    fix: code styling

--- src/server.cpp
+++ src/server.cpp
@@ -1,78 +1,81 @@
-#include <iostream>
-#include <cstdlib>
-#include <string>
-#include <cstring>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+	#include <iostream>
+	#include <cstdlib>
+	#include <string>
+	#include <cstring>
+	#include <unistd.h>
+	#include <sys/types.h>
+	#include <sys/socket.h>
+	#include <arpa/inet.h>
+	#include <netdb.h>
 
-int main(int argc, char **argv) {
+	int main(int argc, char **argv) {
 
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-  
-  std::cout << "Logs from your program will appear here!\n";
+	std::cout << std::unitbuf;
+	std::cerr << std::unitbuf;
+	
+	std::cout << "Logs from your program will appear here!\n";
 
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
-  }
-  //
-  // // Since the tester restarts your program quite often, setting SO_REUSEADDR
-  // // ensures that we don't run into 'Address already in use' errors
-  int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-    std::cerr << "setsockopt failed\n";
-    return 1;
-  }
-  //
-  struct sockaddr_in server_addr;
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(4221);
-  //
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 4221\n";
-    return 1;
-  }
-  //
-  int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
-    return 1;
-  }
-  //
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-  
-  std::cout << "Waiting for a client to connect...\n";
-  
-  int client = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd < 0) {
+	std::cerr << "Failed to create server socket\n";
+	return 1;
+	}
+	//
+	// // Since the tester restarts your program quite often, setting SO_REUSEADDR
+	// // ensures that we don't run into 'Address already in use' errors
+	int reuse = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+		std::cerr << "setsockopt failed\n";
+		return 1;
+	}
+	//
+	struct sockaddr_in server_addr;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_port = htons(4221);
+	//
+	if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+		std::cerr << "Failed to bind to port 4221\n";
+		return 1;
+	}
+	//
+	int connection_backlog = 5;
+	if (listen(server_fd, connection_backlog) != 0) {
+		std::cerr << "listen failed\n";
+		return 1;
+	}
+	//
+	struct sockaddr_in client_addr;
+	int client_addr_len = sizeof(client_addr);
+	
+	std::cout << "Waiting for a client to connect...\n";
+	
+	int client = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
 
-  std::string client_message(1024, '\0');
+	std::string client_message(1024, '\0');
 
-  ssize_t brecvd = recv(client, (void *)&client_message[0], client_message.max_size(), 0);
+	ssize_t brecvd = recv(client, (void *)&client_message[0], client_message.max_size(), 0);
 
-  if(client_message.starts_with("GET /echo/")){
-    int i;
-    std::string x;
-    for(i = 0; client_message[i+10] != ' '; i++){
-      x += client_message[i+10];
-    }
-    std::string message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+std::to_string(x.length())+"\r\n\r\n"+x;
-    send(client, message.c_str(), message.length(), 0);
+	std::string message;
+	if(client_message.starts_with("GET /echo/")){
+		int i;
+		std::string x;
+		for(i = 0; client_message[i+10] != ' '; i++){
+		x += client_message[i+10];
+		}
+		message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+std::to_string(x.length())+"\r\n\r\n"+x;
+	}else if(client_message.starts_with("GET /user-agent")){
+		
+	}else if(client_message.starts_with("GET / HTTP/1.1\r\n")){
+		message =  "HTTP/1.1 200 OK\r\n\r\n";
+	}else{
+		message = "HTTP/1.1 404 Not Found\r\n\r\n";
+	}
 
-  }else{
-    std::string message = client_message.starts_with("GET / HTTP/1.1\r\n") ? "HTTP/1.1 200 OK\r\n\r\n" : "HTTP/1.1 404 Not Found\r\n\r\n";
+	send(client, message.c_str(), message.length(), 0);
+	std::cout << "Client connected\n";
+	
+	close(server_fd);
 
-    send(client, message.c_str(), message.length(), 0);
-  }
-  std::cout << "Client connected\n";
-  
-  close(server_fd);
-
-  return 0;
-}
+	return 0;
+	}
