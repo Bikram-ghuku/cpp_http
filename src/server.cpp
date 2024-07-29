96bb69838591675fe325aa23d5da2a24fed771b6
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-29 19:43:34 +0530

    feat: made multi-threaded

--- src/server.cpp
+++ src/server.cpp
@@ -9,47 +9,9 @@
 #include <netdb.h>
 #include <thread>
 
-int main(int argc, char **argv) {
+int serve(int client){
 
-	std::cout << std::unitbuf;
-	std::cerr << std::unitbuf;
-	
-	std::cout << "Logs from your program will appear here!\n";
 
-	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd < 0) {
-	std::cerr << "Failed to create server socket\n";
-		return 1;
-	}
-
-	int reuse = 1;
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-		std::cerr << "setsockopt failed\n";
-		return 1;
-	}
-
-	struct sockaddr_in server_addr;
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = INADDR_ANY;
-	server_addr.sin_port = htons(4221);
-
-	if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-		std::cerr << "Failed to bind to port 4221\n";
-		return 1;
-	}
-
-	int connection_backlog = 5;
-	if (listen(server_fd, connection_backlog) != 0) {
-		std::cerr << "listen failed\n";
-		return 1;
-	}
-
-	struct sockaddr_in client_addr;
-	int client_addr_len = sizeof(client_addr);
-	
-	std::cout << "Waiting for a client to connect...\n";
-	
-	int client = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
 
 	std::string client_message(1024, '\0');
 
@@ -87,7 +49,62 @@ int main(int argc, char **argv) {
 
 	send(client, message.c_str(), message.length(), 0);
 	std::cout << "Client connected\n";
+
+
 	
+	return 0;
+}
+
+int main(int argc, char **argv) {
+
+	std::cout << std::unitbuf;
+	std::cerr << std::unitbuf;
+	
+	std::cout << "Logs from your program will appear here!\n";
+
+	
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd < 0) {
+		std::cerr << "Failed to create server socket\n";
+		return 1;
+	}
+
+	int reuse = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+		std::cerr << "setsockopt failed\n";
+		return 1;
+	}
+
+	struct sockaddr_in server_addr;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_port = htons(4221);
+
+	if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+		std::cerr << "Failed to bind to port 4221\n";
+		return 1;
+	}
+
+	int connection_backlog = 5;
+	if (listen(server_fd, connection_backlog) != 0) {
+		std::cerr << "listen failed\n";
+		return 1;
+	}	
+
+	struct sockaddr_in client_addr;
+	int client_addr_len = sizeof(client_addr);
+	
+	std::cout << "Waiting for a client to connect...\n";
+	int client;
+	
+	while (1)
+	{
+		client = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+		std::cout << "Client connected\n";
+		std::thread th(serve, client);
+		th.detach();
+	}
+
 	close(server_fd);
 
 	return 0;
