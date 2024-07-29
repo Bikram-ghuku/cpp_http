455714045eadeb2ef321f1deec29ddf8d74586e2
Bikram Ghuku <bikramghuku05@gmail.com>
2024-07-29 19:11:00 +0530

    refactor: cleanup codebase

--- src/server.cpp
+++ src/server.cpp
@@ -20,31 +20,29 @@
 	std::cerr << "Failed to create server socket\n";
 	return 1;
 	}
-	//
-	// // Since the tester restarts your program quite often, setting SO_REUSEADDR
-	// // ensures that we don't run into 'Address already in use' errors
+
 	int reuse = 1;
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
 		std::cerr << "setsockopt failed\n";
 		return 1;
 	}
-	//
+
 	struct sockaddr_in server_addr;
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = INADDR_ANY;
 	server_addr.sin_port = htons(4221);
-	//
+
 	if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
 		std::cerr << "Failed to bind to port 4221\n";
 		return 1;
 	}
-	//
+
 	int connection_backlog = 5;
 	if (listen(server_fd, connection_backlog) != 0) {
 		std::cerr << "listen failed\n";
 		return 1;
 	}
-	//
+
 	struct sockaddr_in client_addr;
 	int client_addr_len = sizeof(client_addr);
 	
@@ -61,7 +59,7 @@
 		int i;
 		std::string x;
 		for(i = 0; client_message[i+10] != ' '; i++){
-		x += client_message[i+10];
+			x += client_message[i+10];
 		}
 		message = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+std::to_string(x.length())+"\r\n\r\n"+x;
 	}else if(client_message.starts_with("GET /user-agent")){
